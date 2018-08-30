ac1f02acebba1c89034a5a6e8e2442babc9ae451
Jared Reed <jreed@caltech.edu>
2018-08-30 13:45:40 -0500

    minor css, removed old files

---
+++ client/components/Layout/HomeUI/HomeUI.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import autobind from 'autobind-decorator'
+
+@autobind class HomeUI extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+
+    }
+  }
+
+
+  componentDidMount() {
+  }
+
+  componentDidUpdate(prevProps) {
+
+  }
+
+
+
+
+
+
+
+  render() {
+    return (
+      <div className="home">
+
+      </div>
+    )
+  }
+}
+
+
+export default HomeUI
--- client/components/Pages/index.js
+++
@@ -1,9 +0,0 @@
-// imports
-import Home from './Home'
-import Login from './Login'
-import Logout from './Logout'
-
-// must include imports here to be accessed via Pages.Whatever later
-export default {
-  Home, Login, Logout
-}
