7e221d3595c0dbd5ebb330675c7858a688faeab9
Aldo Sanchez <josealdosanchez@gmail.com>
2018-09-11 16:53:47 -0500

    Initial img upload

--- server/api/api.js
+++ server/api/api.js
@@ -4,17 +4,25 @@ import express from 'express'
 import db from '../lib/db'
 import appConfig from '../../app-config.js'
 import path from 'path'
-
+import fs from 'fs'
+import multer from 'multer'
+import { addObject, signedURL, deleteObject } from '../aws';
 var well = require('./pozo')
 var intervencion = require('./intervenciones')
 
 const db_con = db.get(appConfig.users.database)
 const app = express()
 
+const upload = multer({
+  limits: { fieldSize: 25 * 1024 * 1024 },
+})
+
 const handleError = (err) => {
   console.error(err)
   return { status: 500, error: true }
 }
+
+app.use(upload.array())
 
 app.get('/ping', (req, res) => {
 	console.log('pong')
@@ -28,6 +36,30 @@ app.get('/getTemplate', (req, res) => {
   res.sendFile(localPath)
 })
 
+app.get('/what', (req, res) => {
+  const buf = fs.readFileSync(path.join(__dirname, '../../', 'screenshot_test.png'))
+  console.log('buf', buf)
+  res.send('done')
+})
+
+app.get('/geturl?', async (req, res) => {
+  const url = await signedURL(req.query.img).catch(reason => console.log(reason))
+  res.send(url)
+})
+
+app.get('/deleteobj', async (req, res) => {
+  const test = await deleteObject(req.query.img)
+  console.log('data', test)
+  res.send('done')
+})
+
+app.post('/testing', (req, res) => {
+  // console.log('this is about to get fucked', req.body)
+  const buf = Buffer.from(req.body.file, 'base64')
+  addObject(buf)
+
+  res.json({ yeah: 'boy' })
+})
 
 app.post('/well', well.create);
 
