3ec8b308f64e99a4b29618487c86b9056d01ab2a
Aldo Sanchez <josealdosanchez@gmail.com>
2018-10-12 13:36:47 -0500

    :construction: Starts tratamiento acido

---
+++ client/redux/reducers/tratamientoAcido.js
@@ -0,0 +1,99 @@
+import { fromJS } from 'immutable'
+
+const initialState = fromJS({ 
+    volAparejo: '',
+    capacidadTotalDelPozo: '',
+    volumenPrecolchonN2: '',
+    volumenSistemaNoReativo: '',
+    volumenSistemaReactivo: '',
+    volumenSistemaDivergente: '',
+    volumenDesplazamientoLiquido: '',
+    volumenDesplazamientoGelLineal: '',  
+    hasErrors: true,
+    moduloYoungArena: '',
+    moduloYoungLutitas: '',
+    relacPoissonArena: '',
+    relacPoissonLutatas: '',
+    gradienteDeFractura: '',
+    densidadDeDisparos: '',
+    diametroDeDisparos: '',
+    tratamientoCompany: '',
+    cedulaData: [{
+        etapa: 1,
+        index: 0,
+        error: true,
+        sistema: '',
+        nombreComercial: '',
+        tipoDeApuntalante: '',
+        concentraciDeApuntalante: '',
+        volLiquid: '',
+        gastoN2: '',
+        gastoLiqudo: '',
+        gastoEnFondo: '',
+        calidad: '',
+        volN2: '',
+        volLiquidoAcum: '',
+        volN2Acum: '',
+        relN2Liq: '',
+        tiempo: '',
+    }]
+})
+
+const tratamientoAcido = (state = initialState, action) => {
+  switch (action.type) {
+    case 'set_mergeTratamientoAcido':
+      return state.mergeDeep(fromJS(action.value))
+    default:
+      return state
+  }
+  // switch (action.type) {
+  //   case 'set_hasErrorsPropuestaAcido':
+  //     return state.set('hasErrors', fromJS(action.value))
+  //   case 'set_intervalo':
+  //       return state.set('intervalo', fromJS(action.value))
+  //   case 'set_longitudDeIntervalo':
+  //       return state.set('longitudDeIntervalo', fromJS(action.value))
+  //   case 'set_volAparejo':
+  //       return state.set('volAparejo', fromJS(action.value))
+  //   case 'set_capacidadTotalDelPozo':
+  //       return state.set('capacidadTotalDelPozo', fromJS(action.value))
+  //   case 'set_volumenPrecolchonN2':
+  //       return state.set('volumenPrecolchonN2', fromJS(action.value))
+  //   case 'set_volumenSistemaNoReativo':
+  //       return state.set('volumenSistemaNoReativo', fromJS(action.value))
+  //   case 'set_volumenSistemaReactivo':
+  //       return state.set('volumenSistemaReactivo', fromJS(action.value))
+  //   case 'set_volumenSistemaDivergente':
+  //       return state.set('volumenSistemaDivergente', fromJS(action.value))
+  //   case 'set_volumenDesplazamientoLiquido':
+  //       return state.set('volumenDesplazamientoLiquido', fromJS(action.value))
+  //   case 'set_volumenDesplazamientoGelLineal':
+  //       return state.set('volumenDesplazamientoGelLineal', fromJS(action.value))
+  //   case 'set_moduloYoungArena':
+  //       return state.set('moduloYoungArena', fromJS(action.value))
+  //   case 'set_moduloYoungLutitas':
+  //       return state.set('moduloYoungLutitas', fromJS(action.value))
+  //   case 'set_relacPoissonArena':
+  //       return state.set('relacPoissonArena', fromJS(action.value))
+  //   case 'set_relacPoissonLutatas':
+  //       return state.set('relacPoissonLutatas', fromJS(action.value))
+  //   case 'set_gradienteDeFractura':
+  //       return state.set('gradienteDeFractura', fromJS(action.value))
+  //   case 'set_densidadDeDisparos':
+  //       return state.set('densidadDeDisparos', fromJS(action.value))
+  //   case 'set_diametroDeDisparos':
+  //       return state.set('diametroDeDisparos', fromJS(action.value))
+  //   case 'set_cedulaDataAcido':
+  //       let newState = state.set('cedulaData', fromJS(action.cedula))
+  //       if (action.volumes !== null) {
+  //           newState = newState.mergeDeep(action.volumes)
+  //       }
+  //       return newState
+  //   case 'set_propuestaCompany':
+  //       return state.set('propuestaCompany', fromJS(action.value))
+  //   default:
+  //     return state
+  // }
+}
+
+export default tratamientoAcido
