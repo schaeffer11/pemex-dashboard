f9020abdf0216878d6ad3e6aff4258d293fb87f7
Jared Reed <schaefferreed@gmail.com>
2018-11-07 11:36:30 -0600

    added small changes to overview tab

--- client/components/Layout/TableroDeControlUI/Overview/Bar.js
+++ client/components/Layout/TableroDeControlUI/Overview/Bar.js
@@ -7,11 +7,16 @@ import classnames from 'classnames'
 export const Bar = ({ estimated, actual }) => {
 
 
-
-  console.log(estimated, actual)
+  let estWidth = estimated > actual ? '100%' : `${estimated / actual * 100}%`
+  let realWidth = estimated > actual ? `${actual / estimated * 100}%` : '100%'
+  let realColor = estimated > actual ? 'red' : '#93D050'
 
   return (
-    <div className={classnames("Bar")} style={{height: '100%'}}>
+    <div className={classnames("Bar")} style={{height: '100%', padding: '33px'}}>
+      <div style={{textAlign: 'left'}}> Real Prod. - {actual.toFixed(0)} bbl/d </div>
+      <div style={{height: '20%', background: realColor, width: realWidth }} />
+      <div style={{textAlign: 'left'}}> Est Prod. - {estimated.toFixed(0)} bbl/d </div>
+      <div style={{height: '20%', background: 'grey', width: estWidth }} />
     </div>
 
   )
