8cf005127022acaa88aba536e98a76d797d1e2f6
Aldo Sanchez <josealdosanchez@gmail.com>
2018-11-29 13:24:31 -0600

    Fixes authorization in diagnosticos and mapeo

--- server/middleware.js
+++ server/middleware.js
@@ -14,7 +14,7 @@ export const sslRedirect = (req, res, next) => {
 }
 
 export const getAuthorization = (req, res, next) => {
-  console.log('getting authorization')
+  console.log('getting authorization', req.url, req.headers.authorization)
   const sessions = listSessions()
   const userSession = sessions.find(elem => `Bearer ${elem.token}` === req.headers.authorization)
   console.log('i found a user session', userSession)
