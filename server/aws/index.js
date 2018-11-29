d8ef6cdbaee7b6f0829247067757e7db8082c8c2
Aldo Sanchez <josealdosanchez@gmail.com>
2018-11-29 02:30:04 -0600

    Starts admin routes and re-routes aws s3

--- server/aws/index.js
+++ server/aws/index.js
@@ -9,7 +9,7 @@ export const signedURL = Key => new Promise((resolve, reject) => {
   // expires in two hours (seconds)
   const params = {
     Key,
-    Bucket: 'pemex-prod-01',
+    Bucket: 'pemex-prod-02',
     Expires: 60 * 60 * 2
   }
   s3.getSignedUrl('getObject', params, (err, url) => {
@@ -23,7 +23,7 @@ export const signedURL = Key => new Promise((resolve, reject) => {
 export const deleteObject = Key => new Promise((resolve, reject) => {
   const params = {
     Key,
-    Bucket: 'pemex-prod-01',
+    Bucket: 'pemex-prod-02',
   }
   s3.deleteObject(params, (err, data) => {
     if (err) {
@@ -44,7 +44,7 @@ export const getBuckets = () => {
 export const addObject = (buf, Key) => new Promise((resolve, reject) => {
   // console.log('what is the key', Key)
   const params = {
-    Bucket: 'pemex-prod-01',
+    Bucket: 'pemex-prod-02',
     Key,
     Body: buf,
     // ContentEncoding: 'base64',
@@ -62,8 +62,8 @@ export const addObject = (buf, Key) => new Promise((resolve, reject) => {
 
 export const copyObject = (oldKey, newKey) => new Promise((resolve, reject) => {
   const params = {
-    Bucket: 'pemex-prod-01',
-    CopySource: `/pemex-prod-01/${oldKey}`, 
+    Bucket: 'pemex-prod-02',
+    CopySource: `/pemex-prod-02/${oldKey}`, 
     Key: newKey,
   }
   s3.copyObject(params, (err, data) => {
